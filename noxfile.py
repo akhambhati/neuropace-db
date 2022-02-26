b7073265cb9555368a12db7bc53a5bdb5375aa5b
Ankit Khambhati <akhambhati@gmail.com>
2022-02-25 23:24:56 -0800

    Initial flake8

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,17 @@
 # noxfile.py
 import nox
 
+locations = "src", "tests", "noxfile.py"
+
 
 @nox.session(python=["3.8", "3.9", "3.10"])
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("pytest", "--cov")
+
+
+@nox.session(python=["3.8", "3.9", "3.10"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
--- tests/test_version.py
+++ tests/test_version.py
@@ -2,5 +2,6 @@
 
 import neuropace_db
 
+
 def test_has_version():
     assert neuropace_db.__version__ is not None
